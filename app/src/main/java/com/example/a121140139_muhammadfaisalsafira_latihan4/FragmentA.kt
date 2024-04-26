a2e1f8f22096acd0f5a4cd7e4e1b79920a5588e1
Muhammad Faisal <MFaisal00359@gmail.com>
2024-04-26 08:34:24 +0700

    test

---
+++ app/src/main/java/com/example/a121140139_muhammadfaisalsafira_latihan4/FragmentA.kt
@@ -0,0 +1,26 @@
+package com.example.a121140139_muhammadfaisalsafira_latihan4
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+
+class FragmentA : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_a, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<View>(R.id.button_go_to_b).setOnClickListener {
+            findNavController().navigate(R.id.action_fragmentA_to_fragmentB)
+        }
+    }
+}
+
